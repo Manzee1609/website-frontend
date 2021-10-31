23f2140b37f9c83494e2295bc41853d5e299ee0b
deeppatel23 <deeppatel2.2312@gmail.com>
2021-10-31 19:56:15 +0530

    all pages added, home page show active event to register

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
-import Webpages from './webpages';
+import {
+  BrowserRouter as Router,
+  Route,
+} from "react-router-dom";
+import Home from './webpages/home';
+import Events from './webpages/events';
+import Teams from './webpages/teams';
+import Blogs from './webpages/blogs';
+import Contact from './webpages/contact';
+
 function App() {
   return (
     <div>
-      <Webpages />
+      <Router>
+            <Route exact path="/" component= {Home} />
+            <Route exact path="/events" component= {Events} />
+            <Route path = "/teams" component = {Teams} />
+            <Route path = "/blogs" component = {Blogs} />
+            <Route path = "/contact" component = {Contact} />
+      </Router>
     </div>
   );
 }
